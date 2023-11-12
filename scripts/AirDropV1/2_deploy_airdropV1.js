b9f5062d77b99422e045f856c3cc6f2b41c514d6
Therock Ani <anitherock44@gmail.com>
2023-11-12 09:34:45 -0800

    first commit

---
+++ scripts/AirDropV1/2_deploy_airdropV1.js
@@ -0,0 +1,67 @@
+const { ethers } = require("hardhat");
+Web3 = require('web3');
+const keccak256 = require("keccak256");
+const { MerkleTree } = require("merkletreejs");
+const fs = require("fs");
+const {QRC20_TOKEN} = process.env;
+
+async function main() {
+    
+	  const qrc20Address = QRC20_TOKEN;
+    const QRC20 = await ethers.getContractFactory("QRC20");
+    const contract = QRC20.attach(qrc20Address);
+
+    // Merkle Root
+    let addresses = [
+      {
+          addr: "<YOUR-PUBLIC-WALLET-ADDRESS>",
+      },
+      {
+          addr: "<YOUR-FRIEND-PUBLIC-WALLET-ADDRESS>"
+      
+      },
+      ];
+  
+      const leafNodes = addresses.map((address) =>
+      keccak256(
+         Buffer.from(address.addr.replace("0x", ""), "hex"),
+      
+      )
+      );
+  
+      const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
+
+      root = merkleTree.getHexRoot();
+
+      const data = {
+        addresses: addresses.map((address) => address.addr),
+        leafNodes: leafNodes,
+        root: root,
+      };
+
+      fs.writeFileSync("tree.json", JSON.stringify(data, null, 2));
+    
+
+      console.log("Deploying Airdrop V1...");
+
+      const AirDropV1 = await ethers.getContractFactory("AirDropV1");
+      
+
+      const dropAmt = Web3.utils.toWei('20', 'ether')
+      const airdrop = await AirDropV1.deploy();
+
+      await airdrop.waitForDeployment();
+
+      console.log("AirdropV1 deployed to:", await airdrop.getAddress())
+      await airdrop.create_airdrop(qrc20Address,dropAmt,root)
+
+      console.log("Funding the Airdrop");
+
+      //mint token
+      const airdropAddress = airdrop.getAddress();
+      const mintAmount = Web3.utils.toWei('2000', 'ether');
+      await contract.mintTo(airdropAddress, mintAmount);
+      console.log("Airdrop Funded...");
+  } 
+
+  main();
